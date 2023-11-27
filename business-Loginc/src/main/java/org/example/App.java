8d62863ef42c2201eb809c31d59c72a080a4c027
stone <2182209930@qq.com>
2023-11-27 10:48:43 +0800

    feat(init):init testMyGit。

---
+++ business-Loginc/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ * @author stone
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
